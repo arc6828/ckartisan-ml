97ffbe782fa68a7bf4096d6ea7b536043b20b63f
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-01-16 11:58:55 +0700

    Update routes_ner.py

--- routes_ner.py
+++ routes_ner.py
@@ -62,7 +62,7 @@ def text():
     content = data.get("content", "")
 
     # LIMIT MAX CHARACTERS
-    max_characters = 1400
+    max_characters = 1000
     limited_content = content[:max_characters]
 
     # Perform NER
