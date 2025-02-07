37b00ef5087fa799de70129b4e664224f6894800
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-02-08 02:05:41 +0700

    Update routes_nlp.py

--- routes_nlp.py
+++ routes_nlp.py
@@ -3,8 +3,11 @@
 from flask import Flask, jsonify, request, Blueprint, render_template
 # from ner import perform_ner_based_on_language
 # from pythainlp.tokenize import word_tokenize
+from googletrans import Translator
 from google.cloud import translate_v2 as translate
 client = translate.Client()
+#Translator
+translator = Translator()
 
 nlp_bp = Blueprint('nlp_bp', __name__)
 
@@ -12,8 +15,35 @@ nlp_bp = Blueprint('nlp_bp', __name__)
 def dashboard():
     return 'NLP'
 
+
 @nlp_bp.route('/translate', methods=['POST'])
-def translate():    
+async def translate():    
+    # text_en = "Hello, how are you?"
+
+    # รับข้อมูลจาก JSON payload หรือ Form data
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = request.form
+
+    # ตรวจสอบข้อมูล
+    if not data:
+        return jsonify({"error": "No JSON provided"}), 400
+
+    # ทำบางอย่างกับข้อมูล
+    input = data.get("input", "")
+
+    # translated_text = client.translate(input, target_language="th")
+    translation = await translator.translate(input, dest = 'th')
+    # print(translated_text["translatedText"])  # สวัสดี, คุณเป็นอย่างไรบ้าง?
+    return {
+        "input" : input,
+        "output" : translation.text,
+    }
+
+
+@nlp_bp.route('/translate/cloud', methods=['POST'])
+def translate_cloud():    
     # text_en = "Hello, how are you?"
 
     # รับข้อมูลจาก JSON payload หรือ Form data
